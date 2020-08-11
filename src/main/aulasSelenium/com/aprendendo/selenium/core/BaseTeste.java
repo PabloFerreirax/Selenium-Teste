ecbe0eb35e01159ce0a1b6aba69c9b8ae16d08d5
PabloFerreirax <pablo.estudoss@gmail.com>
2020-08-10 23:53:26 -0300

    Agora ao final dos testes sera tirado uma Screenshot e colocado no diretorio

--- src/main/aulasSelenium/com/aprendendo/selenium/core/BaseTeste.java
+++ src/main/aulasSelenium/com/aprendendo/selenium/core/BaseTeste.java
@@ -1,13 +1,29 @@
 package com.aprendendo.selenium.core;
 
+import org.apache.tools.ant.util.FileUtils;
 import org.junit.After;
+import org.junit.Rule;
+import org.junit.rules.TestName;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
+import java.io.File;
+import java.io.IOException;
+
+import static com.aprendendo.selenium.core.DriverFactory.getDriver;
 import static com.aprendendo.selenium.core.DriverFactory.killDriver;
 
 public class BaseTeste {
 
+    @Rule
+    public TestName testName = new TestName();
+
     @After
-    public void finaliza(){
+    public void finaliza() throws IOException {
+        TakesScreenshot ss = (TakesScreenshot) getDriver();
+        File arquivo = ss.getScreenshotAs(OutputType.FILE);
+        FileUtils.getFileUtils().copyFile(arquivo, new File("target"+ File.separator +"Screenshot"
+                + File.separator + testName.getMethodName()+".jpg"));
         if(Propriedades.FECHAR_BROWSER){
             killDriver();
         }
--- src/main/aulasSelenium/com/aprendendo/selenium/core/Propriedades.java
+++ src/main/aulasSelenium/com/aprendendo/selenium/core/Propriedades.java
@@ -1,5 +1,6 @@
 package com.aprendendo.selenium.core;
 
 public class Propriedades {
+
     public static boolean FECHAR_BROWSER = true; //tatica prerigosa, so deve ser usada em testes e não em ambiente real
 }
