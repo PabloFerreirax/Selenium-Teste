1b239c8fae374baed500376cfbc9250da99a6733
PabloFerreirax <pablo.estudoss@gmail.com>
2020-08-11 00:04:27 -0300

    Agora tanto o FirefoxDriver quanto GoogleDriver podem ser usados

--- src/main/aulasSelenium/com/aprendendo/selenium/core/DriverFactory.java
+++ src/main/aulasSelenium/com/aprendendo/selenium/core/DriverFactory.java
@@ -2,6 +2,7 @@ package com.aprendendo.selenium.core;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 
@@ -12,9 +13,19 @@ public class DriverFactory {
 
     public static WebDriver getDriver(){
         if(driver == null){
-            System.setProperty("webdriver.chrome.driver", //diz o tipo
-                    "C:\\Users\\pablo\\OneDrive\\Documentos\\TrabalhoHomeOffice\\Drivers\\chromedriver.exe"); //diz onde esta
-            driver = new ChromeDriver(); // instancia o tipo agora
+            switch (Propriedades.browser){
+                case CHROME:
+                    System.setProperty("webdriver.chrome.driver", //diz o tipo
+                            "C:\\Users\\pablo\\OneDrive\\Documentos\\TrabalhoHomeOffice\\Drivers\\chromedriver.exe"); //diz onde esta
+                    driver = new ChromeDriver(); // instancia o tipo agora
+                    break;
+                case FIREFOX:
+                    System.setProperty("webdriver.gecko.driver", //diz o tipo
+                            "C:\\Users\\pablo\\OneDrive\\Documentos\\TrabalhoHomeOffice\\Drivers\\geckodriver.exe"); //diz onde esta
+                    driver = new FirefoxDriver();
+                    break;
+            }
+
         }
         return driver;
     }
